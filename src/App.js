07e454b7ac0021ee9582214abbbf2939fdf2b894
Souvik011 <iamsouvik011@gmail.com>
2023-07-06 23:45:58 +0530

    Summary Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
-import Header from './Components/Header/Header';
+import React, { Fragment } from "react";
+
+import "./App.css";
+import Header from "./Components/Header/Header";
+import Description from "./Components/Meals/Description";
 
 function App() {
-  return (<Header />);
+  return (
+    <Fragment>
+      <Header />
+      <Description />
+    </Fragment>
+  );
 }
 
 export default App;
